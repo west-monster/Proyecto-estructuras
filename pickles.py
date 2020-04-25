f90a0ee86f5aae06602a5eb5ef4c656520944a31
personapardo <50342289+personapardo@users.noreply.github.com>
2020-04-25 10:39:55 -0500

    p

---
+++ pickles.py
@@ -0,0 +1,40 @@
+import numpy as np
+from tarea import almacenamiento
+import pickle
+
+def crearSesion(nombre):
+
+	objeto=almacenamiento()
+	archivo=open(nombre+".pickle","wb")
+	pickle.dump(objeto,	archivo)
+	archivo.close()
+
+def cargarObjeto(datosLista,nombreSesion):
+	
+	archivo=open(nombreSesion+".pickle","rb")
+	almacen=pickle.load(archivo)
+	almacen.add(datosLista)
+	archivo.close()
+
+	archivo=open(nombreSesion+".pickle","wb")
+	pickle.dump(almacen,archivo)
+	archivo.close()
+
+
+def borrarObjeto(nombreObjeto,nombreSesion):
+	
+	archivo=open(nombreSesion+".pickle","rb")
+	almacen=pickle.load(archivo)
+	almacen.delet(nombreObjeto)
+	archivo.close()
+
+	archivo=open(nombreSesion+".pickle","wb")
+	pickle.dump(almacen,archivo)
+	archivo.close()
+
+def buscarObjeto(nombreObjeto,nombreSesion):
+	
+	archivo=open(nombreSesion+".pickle","rb")
+	almacen=pickle.load(archivo)
+	archivo.close()
+	return almacen.searcher(nombreObjeto)
