f60d459e9609a57bb037bf3e26681b53aa3bd9cb
personapardo <dpardogo@unal.edu.co>
2020-04-25 13:29:28 -0500

    Update tarea.py

--- tarea.py
+++ tarea.py
@@ -11,41 +11,41 @@ class almacenamiento:
     def __init__(self):
         self.data = np.zeros((50,) ,dtype=object)
         self.capacity = 50
-        self.tam = 0
-        self.nom = 1
-        self.dispo = []
-        self.jisho = {"nombre": 0}
+        self.size = 0
+        self.references = 1
+        self.freeSpace = []
+        self.dictNames = {"nombre": 0}
 
     def add(self, x):
-        if self.tam == self.capacity:
+        if self.size == self.capacity:
             self.capacity *= 2
             newdata = np.zeros((self.capacity,),dtype=object)
-            newdata[:self.tam] = self.data
+            newdata[:self.size] = self.data
             self.data = newdata
-        if len(self.dispo) != 0:
+        if len(self.freeSpace) != 0:
           
-          if x[0] in self.jisho.keys():
+          if x[0] in self.dictNames.keys():
             temp = self.data[self.search(x[0])]
             temp[3] = x[3] + temp[3]
           else:
-            self.jisho[x[0]] = self.nom
-            self.data[self.dispo[0]] = x
-            self.nom += 1
-            self.dispo.pop(0)
+            self.dictNames[x[0]] = self.references
+            self.data[self.freeSpace[0]] = x
+            self.references += 1
+            self.freeSpace.pop(0)
 
         else:
-          if x[0] in self.jisho.keys():
+          if x[0] in self.dictNames.keys():
             temp = self.data[self.search(x[0])]
             temp[3] = x[3] + temp[3]
           else:
-            self.jisho[x[0]] = self.nom
-            self.data[self.tam] = x
-            self.tam += 1
-            self.nom += 1
+            self.dictNames[x[0]] = self.references
+            self.data[self.size] = x
+            self.size += 1
+            self.references += 1
 
     def search(self,x):
-      if x in self.jisho.keys():
-        for i in range(self.tam):
+      if x in self.dictNames.keys():
+        for i in range(self.size):
           y = self.data[i]
           if y[0] == x:
             return i
@@ -57,15 +57,15 @@ class almacenamiento:
       print(self.data[self.search(x)])
 
     def delet(self,x):
-      if x in self.jisho.keys():
-        for i in range(self.tam):
+      if x in self.dictNames.keys():
+        for i in range(self.size):
           y = self.data[i]
           #posible error cuando self.data == 0
           if y[0] == x:
             self.data[i] = 0
-            self.dispo.append(i)
+            self.freeSpace.append(i)
             break
-        self.jisho.pop(x)
+        self.dictNames.pop(x)
       else:
         print("No se encontró el item")
 
