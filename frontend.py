e0ce9cede43be290c6140f7690365136c8ce74b8
TheComputerCat <jusanchez@unal.edu.co>
2020-04-25 21:09:55 -0500

    new front

--- frontend.py
+++ frontend.py
@@ -3,10 +3,12 @@ from os import system
 from estructuras import *
 from pickles import *
 from time import time 
+from datetime import date
+from prettytable import PrettyTable
 def option():
     while True:
-        menuElements = ["Menú principal", "- l ----- Añadir","- c ----- Crear categoria", "- a ----- Cargar data Auto", "- m ----- Cargar data manual",
-         "- b ----- Buscar", "- f ----- Filtrar", "- d ----- Eliminar todo", "- e ----- Eliminar" , "- q ----- Salir"]
+        menuElements = ["Menú principal", "- l ----- Añadir","- a ----- Cargar data Auto", "- g ----- Guardar",
+         "- b ----- Buscar", "- d ----- Eliminar todo", "- e ----- Eliminar" , "- q ----- Salir"]
         #Print menu with format
         print(menuElements[0].center(os.get_terminal_size().columns,'-'),  end = '')
 
@@ -18,20 +20,36 @@ def option():
         answer = input() 
         if answer == 'l':
             load()
-        elif answer == "c":
-            createCategory()
         elif answer == "a": 
-            loadDataAuto()  
-        elif answer == "m":
-            loadDataManual()  
+            loadDataAuto()
         elif answer == "b":
-            search()  
-        elif answer == "f":
-            filterMenu()
+            search()
         elif answer == "d":
-            deleteEverything()  
+            deleteEverything() 
         elif answer == "e":
             delete()
+        elif answer.split()[0] == "DEV":
+            if len(answer.split()) > 1:
+                if answer.split()[1] == "--help":
+                    print("-l agregar masivamente".ljust(23).center(os.get_terminal_size().columns),  end = '')
+                    print("-d eliminar masivamente".ljust(23).center(os.get_terminal_size().columns),  end = '')
+                    print("-s buscar masivamente".ljust(23).center(os.get_terminal_size().columns),  end = '')
+                    print("Presione enter para regresar el menu".center(os.get_terminal_size().columns))
+                    _ = input()
+                    _ = system('cls')
+                elif answer.split()[1] == "-l":
+                    massiveLoad()
+                elif answer.split()[1] == "-d":
+                    massiveDelete()
+                elif answer.split()[1] == "-s":
+                    masiveSearch()
+                else:
+                    print("Valor incorrecto, --help para ver opciones".center(os.get_terminal_size().columns,' '))
+            else:
+                print("Parametro esperado".center(os.get_terminal_size().columns,' '))  
+                print("Presione enter para regresar el menu".center(os.get_terminal_size().columns))
+                _ = input()
+                _ = system('cls') 
         elif answer == "q":
             _ = system('cls') 
             print("Hasta pronto".center(os.get_terminal_size().columns,'*'))
@@ -40,17 +58,18 @@ def option():
         else:
             _ = system('cls') 
             print("La opcion es incorrecta, ingresela de nuevo")
+
 def notDefined():
     _ = system('cls') 
     print("Lo sentimos, esta utilidad estará disponible en futuras versiones.".center(os.get_terminal_size().columns))
     print("Presione enter para regresar el menu".center(os.get_terminal_size().columns))
     _ = input()
     _ = system('cls') 
+
 def load():
     amnt = int(input("Ingrese el numero de elementos a cargar: "))
     
     almacen=cargarSesion("Default")
-    t0 = time()
     for i in range(amnt):
         lista=[]
       
@@ -59,13 +78,12 @@ def load():
         lista.append(int(input("Ingrese codigo de barras: ")))
         lista.append(int(input("Ingrese cantidad: ")))
 
-        lista.append("{}/{}/{}".format(today.day,today.month,today.year))
+        lista.append(date.today())
         #x = randomword(6)
-        #lista = [x,23,i,1,"12-02-12"] #usar para pruebas random
-        almacen.add(almacen,lista)
-    guardarSesion("Default")
-    tf = time()
-    print("El tiempo para añadir ", amnt, " elementos fue de: ", tf - t0,"s" )
+        #lista = [x,23,i,1,"03-08-1917"] #usar para pruebas random
+        almacen.add(lista)
+    guardarSesion("Default", almacen)
+    print("Elementos añadidos")
     print("Presione enter para regresar el menu".center(os.get_terminal_size().columns))
     _ = input()
     _ = system('cls') 
@@ -76,41 +94,122 @@ def delete():
     almacen=cargarSesion("Default")
 
     for _ in range(amnt):
-        almacen.delete("Ingrese nombre de objeto a eliminar: ")
+        toDelete = input("Ingrese nombre de objeto a eliminar: ")
+        almacen.delet(toDelete)
 
-    guardarSesion(almacen,"Default")
+    guardarSesion("Default", almacen)
+
+    print("El tiempo para eliminados")
+    print("Presione enter para regresar el menu".center(os.get_terminal_size().columns))
+    _ = input()
+    _ = system('cls')  
+
+def search():
+    amnt = int(input("Ingrese el numero de elementos a buscar: "))
+    almacen=cargarSesion("Default")
+    searched = 0
+    while searched < amnt:
+        toSearch = input()
+        elmt = almacen.search(toSearch)
+        searched +=1
+        if elmt != None:
+            printElement(elmt,almacen)
+
+    guardarSesion("Default", almacen)
+    print("Presione enter para regresar el menu".center(os.get_terminal_size().columns))
+    _ = input()
+    _ = system('cls') 
+
+def massiveLoad():
+    _ = system('cls')
+    amnt = int(input("Ingrese el numero de elementos a cargar: "))
+    almacen=cargarSesion("Default")
+    t0 = time()
+    for i in range(amnt):
+        x = randomword(6)
+        lista = [x,23,i,1,"12-02-12"]
+        almacen.add(lista)
+    guardarSesion("Default", almacen)
+    tf = time()
+    print("El tiempo para añadir ", amnt, " elementos fue de: ", tf - t0,"s" )
+    print("Presione enter para regresar el menu".center(os.get_terminal_size().columns))
+    _ = input()
+    _ = system('cls')
+
+def massiveDelete():
+    amnt = int(input("Ingrese el numero de elementos a eliminar: "))
+    almacen=cargarSesion("Default")
+    for i in range(amnt):
+        x = randomword(6)
+        lista = [x,23,i,1,"03-08-1917"]
+        almacen.add(lista)
+    guardarSesion("Default", almacen)
+    deleted = 0
+    t0 = time()
+    while deleted < amnt:
+        almacen.delet(list(almacen.dictNames.keys())[0])
+        deleted +=1
+    guardarSesion("Default", almacen)
+    tf = time()
 
     print("El tiempo para eliminar ", amnt, " elementos fue de: ", tf - t0,"s" )
     print("Presione enter para regresar el menu".center(os.get_terminal_size().columns))
     _ = input()
-    _ = system('cls')  
-def search():
+    _ = system('cls')
+
+def masiveSearch():
     amnt = int(input("Ingrese el numero de elementos a buscar: "))
     almacen=cargarSesion("Default")
-
+    for i in range(amnt):
+        x = randomword(6)
+        lista = [x,23,i,1,"03-08-1917"]
+        almacen.add(lista)
+    guardarSesion("Default", almacen)
     searched = 0
     t0 = time()
     while searched < amnt:
-        almacen.search("Ingrese nombre de objeto a buscar: ")
+        almacen.search(list(almacen.dictNames.keys())[0])
         searched +=1
+    guardarSesion("Default", almacen)
     tf = time()
-    guardarSesion(almacen,"Default")
+
     print("El tiempo para buscar ", amnt, " elementos fue de: ", tf - t0,"s" )
     print("Presione enter para regresar el menu".center(os.get_terminal_size().columns))
     _ = input()
-    _ = system('cls') 
-def createCategory():
-    notDefined()
-def loadDataAuto():
-    notDefined()
-def loadDataManual():
-    notDefined()
-def filterMenu():
-    notDefined()
-def deleteEverything():
-    notDefined()    
+    _ = system('cls')
 
+def loadDataAuto():
+    _ = system('cls')
+    almacen = cargarSesion("Default")
+    t = PrettyTable(['Nombre', 'Precio', 'Codigo de barras', 'Cantidad', 'Fecha de agregado'])
+    for x in almacen.dictNames:
+        if almacen.data[int(almacen.dictNames.get(x))] != 0:
+            t.add_row(almacen.data[int(almacen.dictNames.get(x))])
+    print(t)
+    print("Presione enter para regresar el menu".center(os.get_terminal_size().columns))
+    _ = input()
+    _ = system('cls')
+
+def deleteEverything():
+    almacen=cargarSesion("Default") 
+    almacen.data = np.zeros((50,) ,dtype=object)
+    almacen.capacity = 50
+    almacen.size = 0
+    almacen.references = 1
+    almacen.freeSpace = []
+    almacen.dictNames = {"nombre": 0}
+    guardarSesion("Default", almacen)
+    print("Datos eliminados".center(os.get_terminal_size().columns))
+    _ = input()
+    _ = system('cls')
+def printElement(index, almacen):
+    t = PrettyTable(['Nombre', 'Precio', 'Codigo de barras', 'Cantidad', 'Fecha de agregado'])
+    if almacen.data[index] != 0:
+        t.add_row(almacen.data[index])
+    print(t)
 if __name__ == '__main__':
     if not os.path.exists("Default.pickle"):
         crearSesion("Default")
     option()
+
+			
